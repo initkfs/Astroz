802839a1a6f2826306d97384bdc297747556a9dd
initkfs <(none)>
2022-03-11 15:30:57 +0300

    Add parameterization for degree angle calculator

--- src/astroz/angle/angle_deg_calc.zig
+++ src/astroz/angle/angle_deg_calc.zig
@@ -1,59 +1,65 @@
 const std = @import("std");
 const math = std.math;
 
-pub fn toFullCircleDeg(degs: f64) f64 {
-    const oneTurnDeg = 360.0;
-    return degs - oneTurnDeg * math.floor(degs / oneTurnDeg);
-}
+pub fn AngleDegCalc(comptime FloatType: type) type {
+    return struct {
+        const Self = @This();
 
-fn toFullCircleAnyRangeDeg(valueDegs: f64, startDeg: f64, endDeg: f64, startDelta: f64, endDelta: f64) f64 {
-    const epsilon = math.epsilon(f64);
+        pub fn toFullCircleDeg(degs: FloatType) FloatType {
+            const oneTurnDeg = 360.0;
+            return degs - oneTurnDeg * math.floor(degs / oneTurnDeg);
+        }
 
-    var mustBeNormalize: f64 = valueDegs;
-    if (math.approxEqRel(f64, mustBeNormalize, startDeg, epsilon) or math.approxEqRel(f64, endDeg, epsilon)) {
-        return mustBeNormalize;
-    }
+        fn toFullCircleAnyRangeDeg(valueDegs: FloatType, startDeg: FloatType, endDeg: FloatType, startDelta: FloatType, endDelta: FloatType) FloatType {
+            const epsilon = math.epsilon(FloatType);
 
-    //TODO there is some chance of looping and unnecessary calculations
-    while (mustBeNormalize < startDeg) {
-        mustBeNormalize += startDelta;
-    }
+            var mustBeNormalize: FloatType = valueDegs;
+            if (math.approxEqRel(FloatType, mustBeNormalize, startDeg, epsilon) or math.approxEqRel(FloatType, endDeg, epsilon)) {
+                return mustBeNormalize;
+            }
 
-    while (mustBeNormalize > endDeg) {
-        mustBeNormalize -= endDelta;
-    }
+            //TODO there is some chance of looping and unnecessary calculations
+            while (mustBeNormalize < startDeg) {
+                mustBeNormalize += startDelta;
+            }
 
-    return mustBeNormalize;
-}
+            while (mustBeNormalize > endDeg) {
+                mustBeNormalize -= endDelta;
+            }
 
-pub fn toFullCircleRangeDeg(valueDeg: f64, startDeg: f64, endDeg: f64) f64 {
-    return toFullCircleAnyRangeDeg(valueDeg, startDeg, endDeg, startDeg, endDeg);
-}
+            return mustBeNormalize;
+        }
 
-pub fn toFullCircleNeg90To90Deg(valueDeg: f64) f64 {
-    return toFullCircleRangeDeg(valueDeg, -90, 90);
-}
+        pub fn toFullCircleRangeDeg(valueDeg: FloatType, startDeg: FloatType, endDeg: FloatType) FloatType {
+            return toFullCircleAnyRangeDeg(valueDeg, startDeg, endDeg, startDeg, endDeg);
+        }
 
-pub fn toFullCircleNeg180To180Deg(valueDeg: f64) f64 {
-    return toFullCircleRangeDeg(valueDeg, -180, 180);
-}
+        pub fn toFullCircleNeg90To90Deg(valueDeg: FloatType) FloatType {
+            return toFullCircleRangeDeg(valueDeg, -90, 90);
+        }
 
-pub fn toFull360CircleNeg180To180Deg(valueDeg: f64) f64 {
-    return toFullCircleRangeDeg(valueDeg, -180, 180, 360, 360);
-}
+        pub fn toFullCircleNeg180To180Deg(valueDeg: FloatType) FloatType {
+            return toFullCircleRangeDeg(valueDeg, -180, 180);
+        }
 
-pub fn hoursToDeg(hours: f64) f64 {
-    const secInDeg: f64 = 360;
-    const seconds: f64 = hours * secInDeg;
-    const secInHour: f64 = 15;
-    var degs = seconds * secInHour;
-    degs = toFullCircleDeg(degs);
-    return degs;
+        pub fn toFull360CircleNeg180To180Deg(valueDeg: FloatType) FloatType {
+            return toFullCircleRangeDeg(valueDeg, -180, 180, 360, 360);
+        }
+
+        pub fn hoursToDeg(hours: FloatType) FloatType {
+            const secInDeg: FloatType = 360;
+            const seconds: FloatType = hours * secInDeg;
+            const secInHour: FloatType = 15;
+            var degs = seconds * secInHour;
+            degs = toFullCircleDeg(degs);
+            return degs;
+        }
+    };
 }
 
 test "Test full circle angle" {
     const expect = std.testing.expect;
     const epsilon = math.epsilon(f64);
 
-    try expect(math.approxEqAbs(f64, toFullCircleDeg(370), 10, epsilon));
+    try expect(math.approxEqAbs(f64, AngleDegCalc(f64).toFullCircleDeg(370), 10, epsilon));
 }
